31738a0072e9d0e6dfb8aa298d674a301cd0822d
Eddie Sheffield <eddie@eddiesheffield.com>
2023-10-26 21:54:36 -0400

    Swap GPIO lines for rows and columns to simplify some of the code a bit Switch to positive logic (e.g. active rows are high) Add tracking for key transitions
    Still have to build in debug without optimizations. Even trying vastly different and weird code arrangements, the row increment will happen too soon. Wwwwhhhhhyyyyy!?!?!

--- m0110a_pico.c
+++ m0110a_pico.c
@@ -12,20 +12,20 @@ const uint COLS = 8;
 const uint CLK_PIN = 26;
 const uint DATA_PIN = 28;
 
-const uint ROW_PINS[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-const uint COL_PINS[] = {10, 11, 12, 13, 14, 15, 16, 17};
+const uint ROW_PINS[] = {8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+const uint COL_PINS[] = {0, 1, 2, 3, 4, 5, 6, 7};
 
 const uint OPT_PIN = 18;
 const uint CMD_PIN = 19;
 const uint LCK_PIN = 20;
 const uint SHF_PIN = 21;
 
-// Rows: GPI0 - GP9 -> output
-const uint ROW_INIT_MASK = 0x000003FF;
-const uint ROW_DIR_MASK = 0x000003FF;
+// Rows: GPI8 - GP17 -> output
+const uint ROW_INIT_MASK = 0x0003FF00;
+const uint ROW_DIR_MASK = 0x0003FF00;
 
-// Cols: GP10 - GP17 -> input
-const uint COL_INIT_MASK = 0x0003FC00;
+// Cols: GP0 - GP7 -> input
+const uint COL_INIT_MASK = 0x000000FF;
 const uint COL_DIR_MASK = 0x00000000;
 
 // Modifiers: GP18 - GP21 -> input
@@ -34,6 +34,8 @@ const uint MOD_DIR_MASK = 0X00000000;
 
 const uint MATRIX_INIT_MASK = ROW_INIT_MASK | COL_INIT_MASK | MOD_INIT_MASK;
 const uint MATRIX_DIR_MASK = ROW_DIR_MASK | COL_DIR_MASK | MOD_DIR_MASK;
+
+char key_states[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 int main()
 {
@@ -53,30 +55,50 @@ int main()
 
     for (uint col = 0; col < COLS; col++)
     {
-        gpio_pull_up(COL_PINS[col]);
+        gpio_pull_down(COL_PINS[col]);
     }
 
+    char row = 0;
     while (1)
     {
-        for(uint r = 0; r < ROWS; r++)
+        char *key_row = (char *)(KEYMAP + (COLS * row));
+        gpio_put_masked(ROW_INIT_MASK, 1 << (8 + row));
+        // slight delay for lines to settle
+        sleep_ms(5);
+
+        volatile uint raw_cols = gpio_get_all();
+        char col_values = raw_cols & COL_INIT_MASK;
+
+        char key_col_states = key_states[row];
+        for (char col = 0; col < COLS; col++)
         {
-            uint row = 1 << r;
-            uint active_row = ~row;
-            gpio_put_masked(ROW_INIT_MASK, active_row);
+            char mask = 1 << col;
+            char key_code = key_row[col];
+            char last_key_state = key_col_states & mask;
+            char cur_key_state = col_values & mask;
 
-            // Read all GPIOs, mask out cols, and shift to LSB
-            uint raw_cols = gpio_get_all();
-            uint masked_cols = raw_cols & COL_INIT_MASK;
-            char col_values = ~(masked_cols >> 10);
-            // printf("RowIdx: %d, Row:  0x%04X, Active row:  0x%04X\n", r, row, active_row);
+            if(last_key_state && !cur_key_state) // key transition from pressed to released
+            {
+                // Set bit 7 to indicate key release
+                key_code |= 0x80;
+            }
+            else if(cur_key_state == last_key_state) // key state unchanged
+            {
+                key_code = KEY_NULL;
+            }
 
-            char key_code = 0xFF;
-            for(char c = 0; c < COLS; c++) {
-                if(col_values & (1 << c)) {
-                    key_code = KEYMAP[r][c];
-                    printf("Row: 0x%04X - [%d, %d] = Key code: 0x%02X\n", row, r, c, key_code);
-                }
+            // No special handling of released to pressed transition necessary
+
+            if(key_code != KEY_NULL)
+            {
+                char* txn = (key_code & 0x80)? "Released" : "Pressed";
+                printf("Key code: %02X %s\n", key_code, txn);
             }
         }
+
+        key_states[row] = col_values;
+        // printf("<Post> RowIdx: %d, Row: 0x%04X, Col_Values: 0x%02X\n", row, row, col_values);
+
+        row = (row + 1) % 10;
     }
 }
